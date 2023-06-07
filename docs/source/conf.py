6c0b92d9e5c7c3f19b3158169578b30d218e5e72
Hector Miranda <hectorMiranda@users.noreply.github.com>
2023-06-07 12:04:39 -0600

    Update conf.py
    Changing project name

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Read the docs - research'
+copyright = 'marcetux'
+author = 'Hector'
 
 release = '0.1'
 version = '0.1.0'
